9d51b1ba5c51086790ce58e10adce4224b846f25
Brandon Meeks <brandon.meeks98@gmail.com>
2026-08-22 21:49:08 -0400

    schema file created, database successfully initiated w schema

--- db/schema.sql
+++ db/schema.sql
@@ -1,1 +1,21 @@
--- TODO
+DROP TABLE IF EXISTS playlists CASCADE;
+DROP TABLE IF EXISTS tracks CASCADE;
+DROP TABLE IF EXISTS playlists_tracks;
+
+CREATE TABLE playlists (
+    id SERIAL PRIMARY KEY,
+    name TEXT NOT NULL,
+    description TEXT NOT NULL
+);
+
+CREATE TABLE tracks (
+    id SERIAL PRIMARY KEY,
+    name INT NOT NULL,
+    duration_ms INT NOT NULL
+);
+
+CREATE TABLE playlists_tracks (
+    id SERIAL PRIMARY KEY,
+    playlist_id INT NOT NULL REFERENCES playlists(id) ON DELETE CASCADE,
+    track_id INT NOT NULL UNIQUE REFERENCES tracks(id) ON DELETE CASCADE
+);
